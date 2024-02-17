43fa97014e85df8c1cfa533714eef14bd4c907e3
yatomikazuchi <caioevilabner@gmail.com>
2024-02-17 10:55:49 -0300

    Added text dialog

--- list.py
+++ list.py
@@ -2,4 +2,7 @@ list = ["Oscar", "User", "Claire", "John", "Dazai"]
 user = input("Hello, who are you?")
 list[1] = user
 que = input("Do you want to see the list of students?")
-print(list)
+if que == "yes":
+    print(list)
+elif que != "yes":
+    print("Ok, that's all!")
