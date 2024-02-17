f4dadca2eed31441009d0a807c436eefa05972e2
yatomikazuchi <caioevilabner@gmail.com>
2024-02-17 11:18:03 -0300

    added alphabetical order to list

--- list.py
+++ list.py
@@ -2,6 +2,7 @@ list = ["Oscar", "User", "Claire", "John", "Dazai"]
 user = input("Hello, who are you?")
 list.insert(0, user)
 list.remove("User")
+list.sort()
 que = input("Do you want to see the list of students?")
 if que == "yes":
     print(list)
