8ce331f28c1fe4ea295c0c21536da18aab2f70ea
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-02-19 09:01:12 -0300

    Small fixes in texts

--- NonGenericCode.py
+++ NonGenericCode.py
@@ -1,17 +1,19 @@
 num1 = input("Hello! I'm here to help you with a simple math operation. Choose the first number: ")
 num2 = input("Nice! Now choose the second number: ")
 op = input("Now, what would you like to do with these numbers? (add, subtract, multiply, divide)")
-if op == "add":
+if op == "add" or "+":
   utimoa = float(num1) + float(num2)
   print("The answer is: " + str(utimoa))
-elif op == "subtract":
+elif op == "subtract" or "-":
   utimos = float(num1) - float(num2)
   print("The answer is: " + str(utimos))
-elif op == "multiply":
+elif op == "multiply" or "*":
   utimom = float(num1) * float(num2)
   print("The answer is: " + str(utimom))
-elif op == "divide":
+elif op == "divide" or "/":
   utimod = float(num1) / float(num2)
   print("The answer is: " + str(utimod))
+else:
+  print("Invalid. Try again.")
 
 # funcionavel B)
