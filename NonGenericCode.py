b908cc164259715ce211e730792c80ee059663ef
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-02-20 09:06:33 -0300

    Big fix

--- NonGenericCode.py
+++ NonGenericCode.py
@@ -1,19 +1,25 @@
+done = False
 num1 = input("Hello! I'm here to help you with a simple math operation. Choose the first number: ")
 num2 = input("Nice! Now choose the second number: ")
-op = input("Now, what would you like to do with these numbers? (add, subtract, multiply, divide)")
-if op == "add" or "+":
-  utimoa = float(num1) + float(num2)
-  print("The answer is: " + str(utimoa))
-elif op == "subtract" or "-":
-  utimos = float(num1) - float(num2)
-  print("The answer is: " + str(utimos))
-elif op == "multiply" or "*":
-  utimom = float(num1) * float(num2)
-  print("The answer is: " + str(utimom))
-elif op == "divide" or "/":
-  utimod = float(num1) / float(num2)
-  print("The answer is: " + str(utimod))
-else:
-  print("Invalid. Try again.")
+while done == False:
+  op = input("Now, what would you like to do with these numbers? (add, subtract, multiply, divide)")
+  if op == "add" or op == "+":
+    utimoa = float(num1) + float(num2)
+    print("The answer is: " + str(utimoa))
+    done = True
+  elif op == "subtract" or op == "-":
+    utimos = float(num1) - float(num2)
+    print("The answer is: " + str(utimos))
+    done = True
+  elif op == "multiply" or op == "*":
+    utimom = float(num1) * float(num2)
+    print("The answer is: " + str(utimom))
+    done = True
+  elif op == "divide" or op == "/":
+    utimod = float(num1) / float(num2)
+    print("The answer is: " + str(utimod))
+    done = True
+  else:
+    print("Invalid. Try again.")
 
 # funcionavel B)
