23a87534d48649e0a6c614f7b3578038290e4634
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-02-22 09:00:46 -0300

    Update and rename Treasure to Treasure.py

---
+++ Treasure.py
@@ -0,0 +1,25 @@
+res = ""
+te = ""
+cabo = False
+map = [
+  
+  [3, 2, 1], 
+  [7, 9, 0], 
+  [8, 4, 6],
+  
+]
+print("Hello and welcome to the teasure hunt! Im gonna show you some cordinates, and then you have to find the teasure!")
+print(str(map))
+while te != "yes":
+  te = input("Have you understood what the map?")
+  if te != "yes":
+    print("Ok, I will show you the map again.\n" + str(map))
+print("Ok then, lets start!")
+print("The treasure your looking for is in the middle of the map, and that number can't be divided")
+while cabo == False:
+  res = input("Where do you think the treasure is?")
+  if res == "0":
+    print("Congrats you have found my bees!")
+    cabo = True
+  else:
+    print("Try again!")
