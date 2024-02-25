1d94d36f0474ec1f46d3dd07b658dbdd07a102de
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-02-24 22:58:37 -0300

    Update ProjectWordle.py

--- ProjectWordle.py
+++ ProjectWordle.py
@@ -1,12 +1,26 @@
-correct = "pie"
+correct1 = "car"
+correct2 = "pie"
+word1 = True
+word2 = False
 guess = ""
 guess_counter = 0
 
-while guess != correct:
-  guess = input("Guess the word: ")
-  if guess != correct:
-    print("Wrong answer! Try again")
-    guess_counter = guess_counter + 1
-  else:
-    print("Congratulations!")
-print("You guessed the word in just " + str(guess_counter) + " guesses \o/!")
+while word1 == true
+  while guess != correct:
+    guess = input("Guess the word: ")
+    if guess != correct:
+      print("Wrong answer! Try again")
+      guess_counter = guess_counter + 1
+    else:
+      print("Congratulations!")
+  print("You guessed the word in just " + str(guess_counter) + " guesses \o/!")
+
+while word2 == true
+  while guess != correct:
+    guess = input("Guess the word: ")
+    if guess != correct:
+      print("Wrong answer! Try again")
+      guess_counter = guess_counter + 1
+    else:
+      print("Congratulations!")
+  print("You guessed the word in just " + str(guess_counter) + " guesses \o/!")
