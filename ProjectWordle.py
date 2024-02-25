e5d8df0b64bb2a94b8781572c4681dd71424978f
yatomikazuchi <caioevilabner@gmail.com>
2024-02-24 23:02:26 -0300

    Update

--- ProjectWordle.py
+++ ProjectWordle.py
@@ -5,22 +5,22 @@ word2 = False
 guess = ""
 guess_counter = 0
 
-while word1 == true
-  while guess != correct:
+while word1 == True:
+  while guess != correct1:
     guess = input("Guess the word: ")
-    if guess != correct:
+    if guess != correct1:
       print("Wrong answer! Try again")
       guess_counter = guess_counter + 1
     else:
       print("Congratulations!")
-  print("You guessed the word in just " + str(guess_counter) + " guesses \o/!")
+      print("You guessed the word in just " + str(guess_counter) + " guesses :D")
 
-while word2 == true
-  while guess != correct:
+while word2 == True:
+  while guess != correct2:
     guess = input("Guess the word: ")
-    if guess != correct:
+    if guess != correct2:
       print("Wrong answer! Try again")
       guess_counter = guess_counter + 1
     else:
       print("Congratulations!")
-  print("You guessed the word in just " + str(guess_counter) + " guesses \o/!")
+      print("You guessed the word in just " + str(guess_counter) + " guesses :D")
