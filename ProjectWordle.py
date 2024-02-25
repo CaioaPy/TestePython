f5d1a221312a083dc3dda25c64689f16d4483093
yatomikazuchi <caioevilabner@gmail.com>
2024-02-24 23:19:13 -0300

    updated

--- ProjectWordle.py
+++ ProjectWordle.py
@@ -4,13 +4,17 @@ word1 = False
 word2 = False
 guess = ""
 guess_counter = 0
-
-fc = input("Hello what type of word would you like to guess? \n You can choose between food and vehicle")
+fc = ""
+fc1 = "vehicle"
+fc2 = "food"
+fc = input("Hello what type of word would you like to guess? \nYou can choose between food and vehicle")
 if fc == "food":
   word2 = True
-elif fc == "vehicle"
+elif fc == "vehicle":
   word1 = True
-while word1 == True:
+else:
+      fc = input("You can only choose food or vehicle!")
+if word1 == True:
   while guess != correct1:
     guess = input("Guess the word: ")
     if guess != correct1:
@@ -20,7 +24,7 @@ while word1 == True:
       print("Congratulations!")
       print("You guessed the word in just " + str(guess_counter) + " guesses :D")
 
-while word2 == True:
+elif word2 == True:
   while guess != correct2:
     guess = input("Guess the word: ")
     if guess != correct2:
