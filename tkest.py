791cef6fc264d34214f35ec6b78e577dae02e652
yatomikazuchi <caioevilabner@gmail.com>
2024-04-07 12:29:29 -0300

    updated

--- tkest.py
+++ tkest.py
@@ -4,7 +4,9 @@ root = Tk()
 
 # create
 l1 = Label(root, text="home\n \n\n\nhello")
+l2 = Label(root, text="hello again!")
 # then putting it on screen
-l1.pack()
+l1.grid(row=0, column=0)
+l2.grid(row=14, column=1)
 
 root.mainloop()
