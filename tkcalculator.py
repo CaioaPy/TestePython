47978dfa9fb9e0b2daab18d70b25f02b7b1a23ce
yatomikazuchi <caioevilabner@gmail.com>
2024-04-08 23:11:17 -0300

    updated

--- tkcalculator.py
+++ tkcalculator.py
@@ -1,11 +1,12 @@
 from tkinter import *
 
 root = Tk()
+root.title("Calculator")
 
 title = Label(root, text="calculator")
-button1 = Button(root, text="1")
-button2 = Button(root, text="2")
-button3 = Button(root, text="3")
+button1 = Button(root, text="1", padx=35, pady=35)
+button2 = Button(root, text="2", padx=35, pady=35)
+button3 = Button(root, text="3", padx=35, pady=35)
 
 
 title.grid(row=0 , column=1)
