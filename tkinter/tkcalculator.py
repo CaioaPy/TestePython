71604b5581b0d34c43e1d89a9893353c1199f77d
yatomikazuchi <caioevilabner@gmail.com>
2024-04-10 18:43:21 -0300

    updated

--- tkinter/tkcalculator.py
+++ tkinter/tkcalculator.py
@@ -3,28 +3,31 @@ from tkinter import *
 root = Tk()
 root.title("Calculator")
 
+e = Entry(root, width=35, borderwidth=3)
+e.grid(row=0, column=0, columnspan=3, padx=10, pady=30)
+
 Vtext = ""
 
-def click1():
-    Vtext = "1"
-def click2():
-    Vtext = "2"
-button1 = Button(root, text="1", padx=35, pady=35, command= click1)
-button2 = Button(root, text="2", padx=35, pady=35, command= click2)
-button3 = Button(root, text="3", padx=35, pady=35)
-button4 = Button(root, text="4", padx=35, pady=35)
-button5 = Button(root, text="5", padx=35, pady=35)
-button6 = Button(root, text="6", padx=35, pady=35)
-button7 = Button(root, text="7", padx=35, pady=35)
-button8 = Button(root, text="8", padx=35, pady=35)
-button9 = Button(root, text="9", padx=35, pady=35)
-button0 = Button(root, text="0", padx=35, pady=35)
-buttonR = Button(root, text="=", padx=35, pady=35)
-buttonAdd = Button(root, text="+", padx=35, pady=35)
-buttonSub = Button(root, text="-", padx=35, pady=35)
-buttonDiv = Button(root, text="/", padx=35, pady=35)
-buttonMul = Button(root, text="*", padx=35, pady=35)
-buttonClear = Button(root, text="C", padx=35, pady=35)
+def click(num):
+    e.insert(0, num)
+    return
+
+button1 = Button(root, text="1", padx=35, pady=35, command=lambda: click(1))
+button2 = Button(root, text="2", padx=35, pady=35, command=lambda: click(2))
+button3 = Button(root, text="3", padx=35, pady=35, command=lambda: click(3))
+button4 = Button(root, text="4", padx=35, pady=35, command=lambda: click(4))
+button5 = Button(root, text="5", padx=35, pady=35, command=lambda: click(5))
+button6 = Button(root, text="6", padx=35, pady=35, command=lambda: click(6))
+button7 = Button(root, text="7", padx=35, pady=35, command=lambda: click(7))
+button8 = Button(root, text="8", padx=35, pady=35, command=lambda: click(8))
+button9 = Button(root, text="9", padx=35, pady=35, command=lambda: click(9))
+button0 = Button(root, text="0", padx=35, pady=35, command=lambda: click(0))
+buttonR = Button(root, text="=", padx=35, pady=35, command=lambda: click())
+buttonAdd = Button(root, text="+", padx=35, pady=35, command=lambda: click())
+buttonSub = Button(root, text="-", padx=35, pady=35, command=lambda: click())
+buttonDiv = Button(root, text="/", padx=35, pady=35, command=lambda: click())
+buttonMul = Button(root, text="*", padx=35, pady=35, command=lambda: click())
+buttonClear = Button(root, text="C", padx=35, pady=35, command=lambda: click())
 
 
 button1.grid(row=3, column=0)
