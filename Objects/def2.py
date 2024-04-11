c3fc972e0c568625ccc4eee89ba9a32366753178
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-04-11 09:03:16 -0300

    Update def2.py

--- Objects/def2.py
+++ Objects/def2.py
@@ -3,3 +3,11 @@ def greet(name):
 
 test = greet("oi")
 print (test)
+
+def teste(num1, num2):
+    print(f"hello {num1}, how youre doing?")
+    print(f"i wissh you best than {num2}!")
+    return
+po = input("numeros (2, 2)")
+po2 = input("2")
+teste(po, po2)
