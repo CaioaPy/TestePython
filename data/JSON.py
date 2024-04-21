fed2625ab0e94bc68bf94fb927eca420df64145d
yatomikazuchi <caioevilabner@gmail.com>
2024-04-21 15:49:09 -0300

    updated

--- data/JSON.py
+++ data/JSON.py
@@ -1,12 +1,21 @@
 import json
 
-data = {
+lista = [
+    {
     "name": "Francis",
     "ID": 59283122,
     "Work": "Unemployed",
     "Employed": False,
     "Likes": ["games", "Series"]
+    },
+    {
+    "name": "Marcus",
+    "ID": 1231262,
+    "Work": "Dev",
+    "Employed": True,
+    "Likes": ["learning", "teaching"]
     }
+]
 
 with open("UserData.json", "w") as f:
-    json.dump(data, f)
+    json.dump(lista, f)
