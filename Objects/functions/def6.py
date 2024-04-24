5391d7f03661a54950e5172f3154ce4031818d58
yatomikazuchi <caioevilabner@gmail.com>
2024-04-23 23:36:35 -0300

    updated

--- Objects/functions/def6.py
+++ Objects/functions/def6.py
@@ -3,3 +3,8 @@ monitor = True
 computer_wires = True
 
 def turn_on():
+    if not tomada:
+        print("a tomada não está ligada")
+        
+    if not monitor:
+        print("o monitor está desligado")
