e3e2b0178ecb308719441f1a9b187dc820f69586
yatomikazuchi <caioevilabner@gmail.com>
2024-04-23 23:38:28 -0300

    updated

--- Objects/functions/def6.py
+++ Objects/functions/def6.py
@@ -4,7 +4,13 @@ computer_wires = True
 
 def turn_on():
     if not tomada:
-        print("a tomada não está ligada")
-        
+        print("a tomada não está ligada.")
+
     if not monitor:
-        print("o monitor está desligado")
+        print("o monitor está desligado.")
+
+    if not computer_wires:
+        print("Os fios do computador não estão ligado corretamente.")
+    
+    else:
+        print("Ligado!")
