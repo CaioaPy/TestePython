486710e05ae09787f92ccd4d97a97caf10be0134
yatomikazuchi <caioevilabner@gmail.com>
2024-04-24 23:24:03 -0300

    updated

--- list2.py
+++ list2.py
@@ -1,7 +1,11 @@
 list = []
 def listadd():
     add = input("what would you like to add to the list?")
-    first = input("that should be added at the first position?")
+    my_bool = input("that should be added at the first position? (yes/no)")
+    if my_bool == "yes":
+        first = True
+    else:
+        first = False
     if first :
         list.insert(0, add)
     else:
