baa97daba9ce814a6ffb0eb603bf42a4701bb862
yatomikazuchi <caioevilabner@gmail.com>
2024-04-26 23:43:05 -0300

    updated

--- saving list with json/list2.py
+++ saving list with json/list2.py
@@ -7,6 +7,7 @@ list.append(data)
 def uploadjson():
     with open("upload.json", "w") as f:
         json.dump(list, f)
+
 def listadd():
     add = input("what would you like to add to the list?")
     my_bool = input("that should be added at the first position? (yes/no)")
@@ -19,10 +20,12 @@ def listadd():
     elif first == False:
         list.append(add)
     uploadjson()
+
 def listaremove():
     remove = input("what would you like to remove from the list?")
     list.remove(remove)
     uploadjson()
+
 match input("do you want to see, add or remove from list?"):
     case "see":
         print(list)
