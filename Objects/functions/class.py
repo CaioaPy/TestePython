5fb942aef69f1fe2f78937c6f46ec9c0eff7cb51
yatomikazuchi <caioevilabner@gmail.com>
2024-06-05 16:25:10 -0300

    updated

--- Objects/functions/class.py
+++ Objects/functions/class.py
@@ -1,10 +1,10 @@
 class Bicicleta:
     def __init__(self, cor, modelo, ano, valor, movimento = False):
-        cor.self = cor
-        modelo.self = modelo
-        ano.self = ano
-        valor.self = valor
-        movimento.self = movimento
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = ano
+        self.valor = valor
+        self.movimento = movimento
 
     def buzinar(self):
         print("*bibi*")
@@ -12,8 +12,11 @@ class Bicicleta:
     def parar(self):
         movimento = False
         print("a bicicleta parou")
-        
+
     def mover(self):
         movimento = True
         print("a bicicleta começou a se mover")
     
+bicicleta1 = Bicicleta("azul", "nova", "2024", 1900)
+
+bicicleta1.buzinar()
