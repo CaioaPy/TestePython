5e7952fd9193612b8f2cef90aefbfe7009a2229e
yatomikazuchi <caioevilabner@gmail.com>
2024-06-05 15:33:52 -0300

    updated

--- unique.py
+++ unique.py
@@ -2,3 +2,8 @@ lista = [2, 4, 3, 2, 4, "a", "a", "c", "b"]
 print(lista)
 lista = set(lista)
 print(lista)
+
+cadastro = {"nome": "ot", "idade" : 12,}
+x = input("cidade: ")
+cadastro["cidade"] = x
+print(cadastro)
