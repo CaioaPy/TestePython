3c603a514e3a2741f46a08aa1250d66730a2bef2
yatomikazuchi <caioevilabner@gmail.com>
2024-06-05 16:44:21 -0300

    updated

--- Objects/functions/class.py
+++ Objects/functions/class.py
@@ -1,13 +1,18 @@
 class Bicicleta:
-    def __init__(self, cor, modelo, ano, valor, movimento = False):
+    def __init__(self, cor, modelo, ano, valor, marcha, movimento = False):
         self.cor = cor
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
+        self.marcha = marcha
         self.movimento = movimento
 
+    #def __str__(self):
+    #    return f"Bicileta: cor = {self.cor}, modelo = {self.modelo}, ano = {self.ano}, valor = {self.valor} e movimento = {self.movimento}"
+    
+    #dict method
     def __str__(self):
-        return f"Bicileta: cor = {self.cor}, modelo = {self.modelo}, ano = {self.ano}, valor = {self.valor} e movimento = {self.movimento}"
+        return f"{self.__class__.__name__}: {", ".join([f'{chave} = {valor}' for chave, valor in self.__dict__.items()])}"
 
     def buzinar(self):
         print("*bibi*")
@@ -20,9 +25,18 @@ class Bicicleta:
         movimento = True
         print("a bicicleta começou a se mover")
 
+    def trocar_marcha(self, marcha2):
+        if self.marcha < marcha2:
+            print("trocando de marcha!")
+            print(f"nova marcha: {marcha2}")
+        else:
+            print("nao é possivel trocar de marcha")
+
     
-bicicleta1 = Bicicleta("azul", "nova", "2024", 1900)
+bicicleta1 = Bicicleta("azul", "nova", "2024", 1900, 2)
 
 bicicleta1.buzinar()
 
 print(bicicleta1)
+
+bicicleta1.trocar_marcha(3)
