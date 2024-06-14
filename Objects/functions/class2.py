49237a270e40fe0d7210be093777366ce8ae2654
yatomikazuchi <caioevilabner@gmail.com>
2024-06-14 18:38:24 -0300

    updated

--- Objects/functions/class2.py
+++ Objects/functions/class2.py
@@ -1,17 +1,16 @@
 class Veiculo:
-    def __init__(self, modelo, ano):
+    def __init__(self, cor, modelo, ano):
         self.modelo = modelo
         self.ano = ano
+        self.cor = cor
 
 class Carro(Veiculo):
-    def __init__(self, cor, modelo, ano):
-        self.cor = cor
-        self.modelo = modelo
-        self.ano = ano
-        self.cor = cor
-        
+    def __init__(self, cor, modelo, ano, carregado):
+        super().__init__(cor, modelo, ano)
+        self.carregado = carregado
+
     def Novacor(cor2):
-        print(f"Cor alterada de {cor} para {cor2}")
+        print(f"Cor alterada de {self.cor} para {cor2}")
 
 carro = Carro("azul", "novo", 1942)
 
