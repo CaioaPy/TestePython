b53e355227afa79b71b7cb190e30273e45abe0c9
yatomikazuchi <caioevilabner@gmail.com>
2024-06-16 17:54:55 -0300

    updated

--- tkinter/icons.py
+++ tkinter/icons.py
@@ -1,12 +1,17 @@
 from tkinter import *
+from PIL import ImageTk,Image
 
 root = Tk()
 root.title("icons")
-root.iconbitmap('C:/Users/User/Downloads/0v0.ico')
+root.iconbitmap('C:/Users/User/Documents/programa acao/TestePython/tkinter/img/0v0.ico')
 
 
 e = Entry(root, borderwidth=3)
 e.pack()
+
+img1 = ImageTk.PhotoImage(Image.open("image-w1280.jpg"))
+img_label = Label(image=img1)
+img_label.pack()
 
 def Regular_click():
     label = Label(root, text="You clicked a button.")
@@ -19,8 +24,10 @@ def grab_click():
 button1 = Button(root, text="The Button", command=Regular_click)
 button2 = Button(root, text="The broken Button", state=DISABLED)
 button3 = Button(root, text="The grab button", command=grab_click)
+quit_button = Button(root, text="Exit", command=root.quit)
 button1.pack()
 button2.pack()
 button3.pack()
+quit_button.pack()
 
 root.mainloop()
