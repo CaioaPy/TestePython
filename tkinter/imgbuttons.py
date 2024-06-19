8b6024ed5b8e3cc58f9d9d4ff70012f5f149cd40
yatomikazuchi <caioevilabner@gmail.com>
2024-06-18 23:46:50 -0300

    updated

--- tkinter/imgbuttons.py
+++ tkinter/imgbuttons.py
@@ -5,9 +5,19 @@ root = Tk()
 root.title("wow")
 root.iconbitmap('C:/Users/User/Documents/programa acao/TestePython/tkinter/img/0v0.ico')
 
-buttonA = Button(text="a")
 imgA = ImageTk.PhotoImage(Image.open("A.JPEG"))
+imgB = ImageTk.PhotoImage(Image.open("B.JPEG"))
+imgC = ImageTk.PhotoImage(Image.open("C.JPEG"))
+imgD = ImageTk.PhotoImage(Image.open("D.JPEG"))
+
+buttonA = Button(text="a", image=imgA)
+buttonB = Button(text="a", image=imgB)
+buttonC = Button(text="a", image=imgC)
+buttonD = Button(text="a", image=imgD)
 
 buttonA.pack()
+buttonB.pack()
+buttonC.pack()
+buttonD.pack()
 
 root.mainloop()
