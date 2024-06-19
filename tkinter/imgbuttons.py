0a8ca44b7653ef19fbdf5bfd82099fd26f0bd9fa
yatomikazuchi <caioevilabner@gmail.com>
2024-06-18 23:42:44 -0300

    updated

--- tkinter/imgbuttons.py
+++ tkinter/imgbuttons.py
@@ -1,2 +1,11 @@
 from tkinter import *
 from PIL import ImageTk,Image
+
+root = Tk()
+root.title("wow")
+icon_img = ImageTk.PhotoImage(Image.open("A.JPEG"))
+root.iconbitmap(icon_img)
+
+
+
+root.mainloop()
